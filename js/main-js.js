af0cf429277c57a688ed6c3ee06fe0660c6d2f06
ghOvidiu <33424843+ghOvidiu@users.noreply.github.com>
2019-10-27 18:51:59 +0200

    Add files via upload

--- js/main-js.js
+++ js/main-js.js
@@ -1,1 +1,17 @@
+function navbarDynamicOpacity() {
+    var scrollPosition = $(document).scrollTop();
+    var opacityLevel = 1;
+    scrollPosition < 1000 ? opacityLevel = '0.'+ (scrollPosition * 100) / 10 : opacityLevel;
+    $('#sticky-navbar').css({
+        'background-color': 'rgba(175, 70, 70, '+ opacityLevel +')'
+    });
+    console.log(scrollPosition, opacityLevel);
+};
 
+$(document).ready(function() {
+    console.log("Main-JS loaded.");
+    $(document).scroll(function() {
+            navbarDynamicOpacity();
+        });
+
+});
